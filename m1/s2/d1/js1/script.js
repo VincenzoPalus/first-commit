e401dd2c473ad1163334f8eeb5f2c095f0de1596
VincenzoPalus <vicgamer89@gmail.com>
2022-10-31 06:35:17 +0100

    epicode

---
+++ m1/s2/d1/js1/script.js
@@ -0,0 +1,24 @@
+/*
+Commenyo su due righe
+*/
+
+//commento ad una riga
+
+/**************************
+ * Direttive di output
+ */
+
+//alert('mostra questa scritta')
+
+console.log('Mostra questa scritta');
+
+console.log
+
+console.log('%c Mostra questa scritta','color:red;background-color:blue');
+
+document.getElementById('test').innerHTML = '<b>Prova</b>';
+
+
+document.write('Ciao')//ayyrnzione. potrebbe eliminare il contenuto della pagina
+
+
